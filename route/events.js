82415d2fbbe82f8ed9207cf74f9a7748b7f4fb4b
JulienChasport <julien.chasport@gmail.com>
2019-03-19 12:05:28 +0100

    messages & events

--- route/events.js
+++ route/events.js
@@ -1,6 +1,173 @@
 module.exports = function(app, db) {
-  app.post('/events', (req, res) => {
-    // You'll create your note here.
-    res.send('Hello')
-  });
+ app.post('/events', function(req,res) {
+	var Date = req.body.Date;
+	var Place = req.body.Place;
+	var Id_nest = req.body.Id_nest;
+	var Nb_people = req.body.Nb_people;
+	var Full = req.body.Full;
+	var Name = req.body.Name;
+	var Description = req.body.Description;
+	console.log( Date + "','" + Place + "','" + Id_nest +"','" + Nb_people + "','" + Full + "','" + Name + "','" + Description);
+	var query = "INSERT INTO events (Date, Place, Id_nest, Nb_people, Full, Name, Description) VALUES ('" + Date + "','" + Place + "','" + Id_nest +"','" + Nb_people + "','" + Full + "','" + Name + "','" + Description + "')"; 
+	db.query(query, function(err, result, fields) {
+		if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end("Success");
+		}
+	});
+});
+
+//ROADS FOR READ 
+//For events : ALL DATA 
+app.get('/events', function(req, res) {     
+	var query = "SELECT * FROM events";    
+	var conditions = ["Date", "Place", "Id_nest", "Nb_people", "Full", "Name", "Description"]; 
+
+	//SELECTION
+    for (var index in conditions) {         
+    	if (conditions[index] in req.query) {             
+    		if (query.indexOf("WHERE") < 0) {                 
+    			query += " WHERE";             
+    		} else {                 
+    			query += " AND";             
+    		} 
+
+            query += " " + conditions[index] + "='" + req.query[conditions[index]] + "'";         
+        }     
+    } 
+
+    //SORTING
+    if ("sort" in req.query) {         
+		var sort = req.query["sort"].split(",");         
+		query += " ORDER BY"; 
+        
+        for (var index in sort) {             
+        	var direction = sort[index].substr(0, 1);             
+        	var field = sort[index].substr(1); 
+            
+            query += " " + field; 
+
+            if (direction == "-")                 
+            	query += " DESC,";             
+            else                 
+            	query += " ASC,";         
+        } 
+        
+        query = query.slice(0, -1);     
+    }
+
+    //FILTERING
+    if ("fields" in req.query) {         
+    	query = query.replace("*", req.query["fields"]);     
+    }
+
+    //PAGINATION
+    if ("limit" in req.query) {         
+    	query += " LIMIT " + req.query["limit"];
+
+        if ("offset" in req.query) {             
+        	query += " OFFSET " + req.query["offset"];         
+        }     
+    }
+
+    db.query(query, function(err, result, fields) {         
+    	if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end(JSON.stringify(result));
+		} 
+    }); 
+
+});  
+
+//ROADS FOR READ 
+//For events : ONE DATA
+app.get('/events/:id', function(req, res) {     
+	var id = req.params.id;
+	var query = "SELECT * FROM events WHERE Id = " +id;    
+	var conditions = ["Date", "Place", "Id_nest", "Nb_people", "Full", "Name", "Description"]; 
+
+	//SELECTION
+    for (var index in conditions) {         
+    	if (conditions[index] in req.query) {             
+    		if (query.indexOf("WHERE") < 0) {                 
+    			query += " WHERE";             
+    		} else {                 
+    			query += " AND";             
+    		} 
+
+            query += " " + conditions[index] + "='" + req.query[conditions[index]] + "'";         
+        }     
+    } 
+
+    //SORTING
+    if ("sort" in req.query) {         
+		var sort = req.query["sort"].split(",");         
+		query += " ORDER BY"; 
+        
+        for (var index in sort) {             
+        	var direction = sort[index].substr(0, 1);             
+        	var field = sort[index].substr(1); 
+            
+            query += " " + field; 
+
+            if (direction == "-")                 
+            	query += " DESC,";             
+            else                 
+            	query += " ASC,";         
+        } 
+        
+        query = query.slice(0, -1);     
+    }
+
+    //FILTERING
+    if ("fields" in req.query) {         
+    	query = query.replace("*", req.query["fields"]);     
+    }
+
+    //PAGINATION
+    if ("limit" in req.query) {         
+    	query += " LIMIT " + req.query["limit"];
+
+        if ("offset" in req.query) {             
+        	query += " OFFSET " + req.query["offset"];         
+        }     
+    }
+
+    db.query(query, function(err, result, fields) {         
+    	if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end(JSON.stringify(result));
+		}
+    }); 
+
+});  
+
+//ROADS FOR DELETE
+//In users table :
+app.delete('/events/:id', function(req,res) {
+	var id = req.params.id;
+	var query = "DELETE FROM events WHERE Id = " +id;
+	db.query(query, function(err, result, fields) {
+		if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end("Success");
+		}
+	});
+});
 };
--- route/messages.js
+++ route/messages.js
@@ -1,6 +1,170 @@
 module.exports = function(app, db) {
-  app.post('/messages', (req, res) => {
-    // You'll create your note here.
-    res.send('Hello')
-  });
+ app.post('/messages', function(req,res) {
+	var Id_send = req.body.Id_send;
+	var Id_receive = req.body.Id_receive;
+	var Date = req.body.Date;
+	var Description = req.body.Description;
+	console.log("'"+ Id_send + "','" + Id_receive + "','" + Date +"','" +Description+"'")
+	var query = "INSERT INTO messages (Id_send, Id_receive, Date, Description) VALUES ('" + Id_send + "','" + Id_receive + "','" + Date +"','" + Description +"')"; 
+	db.query(query, function(err, result, fields) {
+		if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end("Success");
+		}
+	});
+});
+
+//ROADS FOR READ 
+//For messages : ALL DATA 
+app.get('/messages', function(req, res) {     
+	var query = "SELECT * FROM messages";    
+	var conditions = ["Id_send", "Id_receive", "Date", "Description", "Id"]; 
+
+	//SELECTION
+    for (var index in conditions) {         
+    	if (conditions[index] in req.query) {             
+    		if (query.indexOf("WHERE") < 0) {                 
+    			query += " WHERE";             
+    		} else {                 
+    			query += " AND";             
+    		} 
+
+            query += " " + conditions[index] + "='" + req.query[conditions[index]] + "'";         
+        }     
+    } 
+
+    //SORTING
+    if ("sort" in req.query) {         
+		var sort = req.query["sort"].split(",");         
+		query += " ORDER BY"; 
+        
+        for (var index in sort) {             
+        	var direction = sort[index].substr(0, 1);             
+        	var field = sort[index].substr(1); 
+            
+            query += " " + field; 
+
+            if (direction == "-")                 
+            	query += " DESC,";             
+            else                 
+            	query += " ASC,";         
+        } 
+        
+        query = query.slice(0, -1);     
+    }
+
+    //FILTERING
+    if ("fields" in req.query) {         
+    	query = query.replace("*", req.query["fields"]);     
+    }
+
+    //PAGINATION
+    if ("limit" in req.query) {         
+    	query += " LIMIT " + req.query["limit"];
+
+        if ("offset" in req.query) {             
+        	query += " OFFSET " + req.query["offset"];         
+        }     
+    }
+
+    db.query(query, function(err, result, fields) {         
+    	if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end(JSON.stringify(result));
+		} 
+    }); 
+
+});  
+
+//ROADS FOR READ 
+//For messages : ONE DATA
+app.get('/messages/:id', function(req, res) {     
+	var id = req.params.id;
+	var query = "SELECT * FROM messages WHERE Id = " +id;    
+	var conditions = ["Id_send", "Id_receive", "Date", "Description", "Id"]; 
+
+	//SELECTION
+    for (var index in conditions) {         
+    	if (conditions[index] in req.query) {             
+    		if (query.indexOf("WHERE") < 0) {                 
+    			query += " WHERE";             
+    		} else {                 
+    			query += " AND";             
+    		} 
+
+            query += " " + conditions[index] + "='" + req.query[conditions[index]] + "'";         
+        }     
+    } 
+
+    //SORTING
+    if ("sort" in req.query) {         
+		var sort = req.query["sort"].split(",");         
+		query += " ORDER BY"; 
+        
+        for (var index in sort) {             
+        	var direction = sort[index].substr(0, 1);             
+        	var field = sort[index].substr(1); 
+            
+            query += " " + field; 
+
+            if (direction == "-")                 
+            	query += " DESC,";             
+            else                 
+            	query += " ASC,";         
+        } 
+        
+        query = query.slice(0, -1);     
+    }
+
+    //FILTERING
+    if ("fields" in req.query) {         
+    	query = query.replace("*", req.query["fields"]);     
+    }
+
+    //PAGINATION
+    if ("limit" in req.query) {         
+    	query += " LIMIT " + req.query["limit"];
+
+        if ("offset" in req.query) {             
+        	query += " OFFSET " + req.query["offset"];         
+        }     
+    }
+
+    db.query(query, function(err, result, fields) {         
+    	if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end(JSON.stringify(result));
+		}
+    }); 
+
+});  
+
+//ROADS FOR DELETE
+//In users table :
+app.delete('/messages/:id', function(req,res) {
+	var id = req.params.id;
+	var query = "DELETE FROM messages WHERE Id = " +id;
+	db.query(query, function(err, result, fields) {
+		if (err) {
+			res.writeHead(404)
+			res.end("Failed");
+		}
+		else {
+			res.writeHead(200)
+			res.end("Success");
+		}
+	});
+});
 };
