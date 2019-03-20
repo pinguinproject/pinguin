a3ccddb42625befb06dae7c5acda379ecb08bd8f
Manu <emmanuel.nguyen14@gmail.com>
2019-03-20 15:17:23 +0100

    With best sql file

--- pinguin.sql
+++ pinguin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 20 mars 2019 à 13:26
+-- Généré le :  mer. 20 mars 2019 à 14:16
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
@@ -165,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Rights` enum('admin','user') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Mail` (`Mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `users`
@@ -173,7 +173,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`Firstname`, `Lastname`, `Completename`, `Birthdate`, `Sex`, `Mail`, `Phone`, `Id`, `Rights`) VALUES
 ('Manu', 'Nguyen', 'Manu Nguyen', '1997-03-14', 0, 'emmanuel.nguyen14@gmail.com', '0620326983', 1, 'admin'),
-('Manu', 'Nguyen', 'Manu Nguyen', '1997-03-14', 0, 'emmanuel.nguyen@gmail.com', '0620326983', 2, 'user');
+('Manu', 'Nguyen', 'Manu Nguyen', '1997-03-14', 0, 'emmanuel.nguyen@gmail.com', '0620326983', 2, 'user'),
+('Manu', 'Nguyen', 'Manu Nguyen', '1997-03-14', 1, 'emmanuel.nguyen143@gmail.com', 'fezjnf', 5, 'admin');
 
 --
 -- Contraintes pour les tables déchargées
